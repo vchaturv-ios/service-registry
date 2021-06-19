f4f65465f53e4b2b74dfcd4cecc9a858034ee4cd
Vishnu Chaturvedi <vishnu.chaturvedi.1002@gmail.com>
2021-06-20 01:12:39 +0530

    Added Service Registry.

---
+++ src/test/java/com/vish/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vish.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
